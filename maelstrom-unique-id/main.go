9677a8b4097e213685db579d933a3b921b6a1764
Vasu Bansal <vasubansal1998@gmail.com>
2024-11-23 19:06:16 +0530

    [Vasu] refactored unique id code

--- maelstrom-unique-id/main.go
+++ maelstrom-unique-id/main.go
@@ -8,23 +8,36 @@ import (
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 )
 
+type Server struct {
+	Node    *maelstrom.Node
+	Counter int
+}
+
+func (s *Server) HandleGenerate(msg maelstrom.Message) error {
+	var body map[string]any
+
+	if err := json.Unmarshal(msg.Body, &body); err != nil {
+		return err
+	}
+
+	body["type"] = "generate_ok"
+	body["id"] = uuid.New().String()
+
+	return s.Node.Reply(msg, body)
+}
+
+func NewServer() *Server {
+	return &Server{
+		Node: maelstrom.NewNode(),
+	}
+}
+
 func main() {
 
-	n := maelstrom.NewNode()
-	n.Handle("generate", func(msg maelstrom.Message) error {
-		var body map[string]any
+	s := NewServer()
+	s.Node.Handle("generate", s.HandleGenerate)
 
-		if err := json.Unmarshal(msg.Body, &body); err != nil {
-			return err
-		}
-
-		body["type"] = "generate_ok"
-		body["id"] = uuid.New().String()
-
-		return n.Reply(msg, body)
-	})
-
-	if err := n.Run(); err != nil {
+	if err := s.Node.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
---
+++ maelstrom-unique-id/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(pwd)/bin
+MAELSTROM_PATH=../maelstrom
+
+mkdir -p $SCRIPT_DIR
+go build -o $SCRIPT_DIR/main
+
+"$MAELSTROM_PATH/maelstrom" test -w unique-ids --bin $SCRIPT_DIR/main --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
